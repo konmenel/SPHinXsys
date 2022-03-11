c3d11e1090a1ae51616cf1c5c2db83489b8ab08e
konmenel <konmenel@gmail.com>
2022-03-11 11:38:20 +0000

    Added more ichec cases

---
+++ tests/user_examples/ichec_case_coarser/src/coarser.h
@@ -0,0 +1,157 @@
+/**
+ * @brief 	SPHinXsys Library.
+ */
+#include "sphinxsys.h"
+
+#define SQRT2 1.41421356237
+/**
+ * @brief Namespace cite here.
+ */
+using namespace SPH;
+
+/**
+ * @brief Basic geometry parameters and numerical setup.
+ */
+const Real DL = 8.0; 								/**< Tank length [m]. */
+const Real DH = 0.5; 								/**< Tank height [m]. */
+const Real WH = 0.4; 								/**< Water block width [m]. */
+const Real WL = 3.0; 								/**< Water block height [m]. */
+const Real WALL_H = 0.2;							/**< Verical wall height [m] */
+const Real WALL_X = 7.0;							/**< Position of the verical wall [m] (x direction) */ 
+const Real particle_spacing_ref = 1.5e-3 * SQRT2; 	/**< Initial reference particle spacing. */
+const Real BW = particle_spacing_ref * 6.0; 		/**< Extending width for BCs. */
+
+/** Domain bounds of the system. */
+BoundingBox system_domain_bounds(Vec2d(-BW, -BW), Vec2d(DL + BW, DH + BW));
+
+/**< Gravity force of fluid. [m/s^2] */
+const Real gravity_g = 9.81;
+
+/**
+ * @brief Material properties of the fluid.
+ */
+const Real rho0_f = 1000.0;								/**< Reference density of fluid [kg/m^3]. */
+const Real U_f = 2.0 * sqrt(gravity_g * WH);			/**< Characteristic velocity [m/s]. */
+const Real c_f = 10.0 * U_f;							/**< Reference sound speed [m/s]. */
+const Real mu_f = 1.0e-6;								/**< Reference dynamic viscocity of fluid [Ns/m^2]. */
+
+/**
+* @brief 	Create a water block shape.
+*/
+std::vector<Vecd> createWaterBlockShape()
+{
+	//geometry
+	std::vector<Vecd> water_block_shape;
+	water_block_shape.push_back(Vecd(0.0, 0.0));
+	water_block_shape.push_back(Vecd(0.0, WH));
+	water_block_shape.push_back(Vecd(WL, WH));
+	water_block_shape.push_back(Vecd(WL, 0.0));
+	water_block_shape.push_back(Vecd(0.0, 0.0));
+	return water_block_shape;
+}
+
+/** 
+* @brief	Create outer wall shape.
+*/
+std::vector<Vecd> createOuterWallShape()
+{
+	std::vector<Vecd> outer_wall_shape;
+	outer_wall_shape.push_back(Vecd(-BW, -BW));
+	outer_wall_shape.push_back(Vecd(-BW, DH + BW));
+	outer_wall_shape.push_back(Vecd(DL + BW, DH + BW));
+	outer_wall_shape.push_back(Vecd(DL + BW, -BW));
+	outer_wall_shape.push_back(Vecd(-BW, -BW));
+	return outer_wall_shape;
+}
+
+/**
+* @brief 	Create inner wall shape.
+*/
+std::vector<Vecd> createInnerWallShape()
+{
+	std::vector<Vecd> inner_wall_shape;
+	inner_wall_shape.push_back(Vecd(0.0, 0.0));
+	inner_wall_shape.push_back(Vecd(0.0, DH));
+	inner_wall_shape.push_back(Vecd(DL, DH));
+	inner_wall_shape.push_back(Vecd(DL, 0.0));
+	inner_wall_shape.push_back(Vecd(0.0, 0.0));
+	return inner_wall_shape;
+}
+/**
+* @brief 	Create outer verical wall shape.
+*/
+std::vector<Vecd> createOuterVerWallShape()
+{
+	std::vector<Vecd> outer_ver_wall_shape;
+	outer_ver_wall_shape.push_back(Vecd(WALL_X, 0.0));
+	outer_ver_wall_shape.push_back(Vecd(WALL_X, WALL_H));
+	outer_ver_wall_shape.push_back(Vecd(DL, WALL_H));
+	outer_ver_wall_shape.push_back(Vecd(DL, 0.0));
+	outer_ver_wall_shape.push_back(Vecd(WALL_X, 0.0));
+	return outer_ver_wall_shape;
+}
+
+/**
+* @brief 	Create inner verical wall shape.
+*/
+std::vector<Vecd> createInnerVerWallShape()
+{
+	std::vector<Vecd> inner_ver_wall_shape;
+	inner_ver_wall_shape.push_back(Vecd(WALL_X + BW, -BW));
+	inner_ver_wall_shape.push_back(Vecd(WALL_X + BW, WALL_H - BW));
+	inner_ver_wall_shape.push_back(Vecd(DL + BW, WALL_H - BW));
+	inner_ver_wall_shape.push_back(Vecd(DL + BW, -BW));
+	inner_ver_wall_shape.push_back(Vecd(WALL_X + BW, -BW));
+	return inner_ver_wall_shape;
+}
+
+/**
+* @brief 	Fluid body definition.
+*/
+class WaterBlock : public FluidBody
+{
+public:
+	WaterBlock(SPHSystem& sph_system, string body_name)
+		: FluidBody(sph_system, body_name)
+	{
+		/** Geomtry definition. */
+		MultiPolygon multi_polygon;
+		std::vector<Vecd> water_block_shape = createWaterBlockShape();
+		multi_polygon.addAPolygon(water_block_shape, ShapeBooleanOps::add);
+		body_shape_.add<MultiPolygonShape>(multi_polygon);
+	}
+};
+
+
+/**
+* @brief 	Case dependent material properties definition.
+*/
+class WaterMaterial : public WeaklyCompressibleFluid
+{
+public:
+	WaterMaterial() : WeaklyCompressibleFluid(rho0_f, c_f, mu_f) {}
+};
+
+
+/**
+ * @brief 	Wall boundary body definition.
+ */
+class WallBoundary : public SolidBody
+{
+public:
+	WallBoundary(SPHSystem &sph_system, string body_name)
+		: SolidBody(sph_system, body_name)
+	{
+		/** Geomtry definition. */
+		MultiPolygon multi_polygon;
+		std::vector<Vecd> outer_shape = createOuterWallShape();
+		std::vector<Vecd> inner_shape = createInnerWallShape();
+		std::vector<Vecd> outer_vertical_wall_shape = createOuterVerWallShape();
+		std::vector<Vecd> inner_vertical_wall_shape = createInnerVerWallShape();
+		multi_polygon.addAPolygon(outer_shape, ShapeBooleanOps::add);
+		multi_polygon.addAPolygon(inner_shape, ShapeBooleanOps::sub);
+		multi_polygon.addAPolygon(outer_vertical_wall_shape, ShapeBooleanOps::add);
+		multi_polygon.addAPolygon(inner_vertical_wall_shape, ShapeBooleanOps::sub);
+		body_shape_.add<MultiPolygonShape>(multi_polygon);
+	}
+};
---
+++ tests/user_examples/ichec_case_coarsest/src/ichec_case_coarsest.cpp
@@ -0,0 +1,129 @@
+/**
+ * @file 	ichec_case_coarse.cpp
+ * @brief 	2D dambreak example for ichec application with coarse resolution.
+ * @author 	Constantinos Menelaou
+ */
+/**
+ * @brief The setup of the case
+ */
+#include "coarsest.h"
+/**
+ * @brief 	Main program starts here.
+ */
+int main(int ac, char *av[])
+{
+	size_t threads;
+	/** Build up context -- a SPHSystem. */
+	if (ac == 1){
+		threads = thread::hardware_concurrency();
+	} else if (ac == 2) {
+		threads = boost::lexical_cast<size_t>(av[1]);
+	} else {
+		cout << ac << " arguments provided by 1 was expected." << endl;
+		return 1;
+	}
+	
+	SPHSystem system(system_domain_bounds, particle_spacing_ref, threads);
+	/** Define external force.*/
+	Gravity gravity(Vecd(0.0, -gravity_g));
+	/** The water block, body, material and particles container. */
+	WaterBlock water_block(system, "WaterBody");
+	FluidParticles fluid_particles(water_block, makeShared<WaterMaterial>());
+	/** The wall boundary, body and particles container. */
+	WallBoundary wall_boundary(system, "Wall");
+	SolidParticles wall_particles(wall_boundary);
+	/** topology */
+	ComplexBodyRelation water_block_complex(water_block, {&wall_boundary});
+	/**
+	 * Methods used for time stepping
+	 */
+	/** Time step initialization, add gravity. */
+	TimeStepInitialization initialize_gravity_to_fluid(water_block, gravity);
+	/** Evaluation of density by summation approach. */
+	fluid_dynamics::DensitySummationFreeSurfaceComplex update_density_by_summation(water_block_complex);
+	/** time step size without considering sound wave speed. */
+	fluid_dynamics::AdvectionTimeStepSize get_fluid_advection_time_step_size(water_block, U_f);
+	/** time step size with considering sound wave speed. */
+	fluid_dynamics::AcousticTimeStepSize get_fluid_time_step_size(water_block);
+	/** pressure relaxation using verlet time stepping. */
+	fluid_dynamics::PressureRelaxationRiemannWithWall pressure_relaxation(water_block_complex);
+	fluid_dynamics::DensityRelaxationRiemannWithWall density_relaxation(water_block_complex);
+	/** Computing viscous acceleration. */
+	fluid_dynamics::ViscousAccelerationWithWall viscous_acceleration(water_block_complex);
+	
+	/** Output. */
+	ofstream fcout("./stdout.out");
+	fcout << "Setting up output...\n";
+
+	/**
+	 * @brief Prepare quantities will be used once only and initial condition.
+	 */
+	/** offset particle position */
+	system.initializeSystemCellLinkedLists();
+	system.initializeSystemConfigurations();
+
+	/** Simulation start here. */
+	fcout << "Setting up main loop...";
+	/** starting time zero. */
+	system.restart_step_ = 0;
+	GlobalStaticVariables::physical_time_ = 0.0;
+	int number_of_iterations = 0;
+	int screen_output_interval = 1000;
+	Real End_Time = 10.0;			 /**< End time. */
+	Real Dt = 0.0;					 /**< Default advection time step sizes. */
+	Real dt = 0.0;					 /**< Default acoustic time step sizes. */
+	Real total_time = 0.0;
+	/** statistics for computing time. */
+	tick_count t1 = tick_count::now();
+	fcout << " DONE\n"
+		  << "Main loop started.\n";
+	fcout.flush();
+	/** Main Loop. */
+	while (GlobalStaticVariables::physical_time_ < End_Time)
+	{
+		/** Acceleration due to viscous force and gravity. */
+		initialize_gravity_to_fluid.parallel_exec();
+
+		Dt = get_fluid_advection_time_step_size.parallel_exec();
+		update_density_by_summation.parallel_exec();
+		viscous_acceleration.parallel_exec();
+
+		/** Dynamics including pressure relaxation. */
+		Real relaxation_time = 0.0;
+		while (relaxation_time < Dt)
+		{
+			pressure_relaxation.parallel_exec(dt);
+			// boulder_damping.parallel_exec(dt);
+			density_relaxation.parallel_exec(dt);
+
+			dt = get_fluid_time_step_size.parallel_exec();
+			relaxation_time += dt;
+			total_time += dt;
+			GlobalStaticVariables::physical_time_ += dt;
+		}
+
+		if (number_of_iterations % screen_output_interval == 0)
+		{
+			fcout << fixed << setprecision(9) << "N=" << number_of_iterations
+					<< "	Total Time = " << total_time
+					<< "	Physical Time = " << GlobalStaticVariables::physical_time_
+					<< "	Dt = " << Dt << "	dt = " << dt << "\n";
+			fcout.flush();
+		}
+		number_of_iterations++;
+		water_block.updateCellLinkedList();
+		wall_boundary.updateCellLinkedList();
+		water_block_complex.updateConfiguration();
+	}
+	tick_count t4 = tick_count::now();
+
+	tick_count::interval_t wall_time, total_run_time;
+	wall_time = t4 - t1;
+	total_run_time = t4 - t1;
+	fcout << "Total wall time for computation: " << wall_time.seconds() << " seconds.\n";
+	fcout << "Total time: " << total_run_time.seconds() << "seconds.\n";
+	
+	fcout.flush();
+	fcout.close();
+	return 0;
+}
