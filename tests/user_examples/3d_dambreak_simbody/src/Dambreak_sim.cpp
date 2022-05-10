f826c3495d15087a9f81824e3f71cae5708b8622
konmenel <konmenel@gmail.com>
2022-05-10 10:41:55 +0100

    Add 3d simbody contact case

---
+++ tests/user_examples/3d_dambreak_simbody/src/Dambreak_sim.cpp
@@ -0,0 +1,224 @@
+/* ---------------------------------------------------------------------------*
+*                       SPHinXsys: 3D dambreak example                        *
+* ----------------------------------------------------------------------------*
+* This is the one of the basic test cases for efficient and accurate time     *
+* integration scheme investigation 							  				  *
+* ---------------------------------------------------------------------------*/
+#include "Dambreak_sim.h"
+
+
+//the main program
+int main()
+{
+
+	//build up context -- a SPHSystem
+	SPHSystem system(system_domain_bounds, resolution_ref);
+	/** Set the starting time. */
+	GlobalStaticVariables::physical_time_ = 0.0;
+	/** Tag for computation from restart files. 0: not from restart files. */
+	system.restart_step_ = 0;
+
+	// //the water block
+	// WaterBlock water_block(system, "WaterBody");
+	// // create fluid particles
+	// FluidParticles fluid_particles(water_block, makeShared<WeaklyCompressibleFluid>(rho0_f, c_f, mu_f));
+
+	//the wall boundary
+	WallBoundary wall_boundary(system, "Wall");
+	//create solid particles
+	SolidParticles wall_particles(wall_boundary);
+
+	//the wall boundary
+	Boulder boulder(system, "Boulder");
+	//create solid particles
+	ElasticSolidParticles boulder_particles(boulder, makeShared<LinearElasticSolid>(rho0_s, poisson, Youngs_modulus));
+	
+	/** topology */
+	// ComplexBodyRelation water_block_complex(water_block, {&wall_boundary, &boulder});
+	BodyRelationInner boulder_inner(boulder);
+	solid_dynamics::CorrectConfiguration boulder_corrected_configuration(boulder_inner);
+
+	//-------------------------------------------------------------------
+	//this section define all numerical methods will be used in this case
+	//-------------------------------------------------------------------
+	//-------- common particle dynamics ----------------------------------------
+	// Gravity gravity(Vec3d(0.0, 0.0, -gravity_g));
+	// TimeStepInitialization initialize_a_fluid_step(water_block, gravity);
+	// //-------- fluid dynamics --------------------------------------------------
+	// //evaluation of density by summation approach
+	// fluid_dynamics::DensitySummationFreeSurfaceComplex update_density_by_summation(water_block_complex);
+	// //time step size without considering sound wave speed
+	// fluid_dynamics::AdvectionTimeStepSize get_fluid_advection_time_step_size(water_block, U_f);
+	// //time step size with considering sound wave speed
+	// fluid_dynamics::AcousticTimeStepSize get_fluid_time_step_size(water_block);
+
+	// //pressure relaxation using verlet time stepping
+	// fluid_dynamics::PressureRelaxationWithWall pressure_relaxation(water_block_complex);
+	// fluid_dynamics::DensityRelaxationWithWall density_relaxation(water_block_complex);
+	// /** Computing viscous acceleration. */
+	// // fluid_dynamics::ViscousAccelerationWithWall viscous_acceleration(water_block_complex);
+	// /** Impose transport velocity. */
+	// fluid_dynamics::TransportVelocityCorrectionComplex transport_velocity_correction(water_block_complex);
+	/** viscous acceleration and transport velocity correction can be combined because they are independent dynamics. */
+	// CombinedInteractionDynamics viscous_acceleration_and_transport_correction(viscous_acceleration, transport_velocity_correction);
+	cout << "Simbody starting..." << endl;
+	// ----------------------------------------------------------------------------
+	//Simbody
+	//-----------------------------------------------------------------------------
+	/** Multi-body system. */
+	/** set up the multi body system. */
+	SimTK::MultibodySystem MBsystem;
+	/** the bodies or matter of the system. */
+	SimTK::SimbodyMatterSubsystem 	matter(MBsystem);
+	/** the forces of the system. */
+	SimTK::GeneralForceSubsystem 	forces(MBsystem);
+	/** the contact syster. */
+	SimTK::ContactTrackerSubsystem  tracker(MBsystem);
+	SimTK::CompliantContactSubsystem contactForces(MBsystem, tracker);
+	contactForces.setTransitionVelocity(1e-3);
+
+	//-----------------------------------------------------------------------------
+	//Simbody forces
+	//-----------------------------------------------------------------------------
+	SimTK::Force::UniformGravity sim_gravity(forces, matter, Vec3d(0.0, 0.0, -gravity_g));
+	/** discrete forces acting on the bodies. */
+	SimTK::Force::DiscreteForces force_on_bodies(forces, matter);
+	
+	/** contanct clique for wall */
+	SimTK::ContactCliqueId clique = SimTK::ContactSurface::createNewContactClique();
+	
+	addSimbodyWallContacts(matter, clique);
+	addCliffContactForSimbody(matter, clique);
+	
+	BoulderSystemForSimbody boulder_multibody(boulder, "BoulderMultibody", boulder.body_shape_);
+	SimTK::Body::Rigid    boulder_info(*boulder_multibody.body_part_mass_properties_);
+
+	addBoulderContactForSimbody(boulder_info);
+
+	SimTK::MobilizedBody::Free boulder_body(matter.Ground(), 
+		SimTK::Transform(Vec3d(VWx - BDx - BDL*0.5, BDy, BDz + BDH*0.5)),
+		boulder_info, SimTK::Transform());
+
+	SimTK::State state = MBsystem.realizeTopology();
+	SimTK::SemiExplicitEuler2Integrator integ(MBsystem);
+	integ.setAccuracy(1e-3);
+	integ.setAllowInterpolation(false);
+	integ.initialize(state);
+
+	solid_dynamics::TotalForceOnSolidBodyPartForSimBody
+		force_on_boulder(boulder, boulder_multibody, MBsystem, boulder_body, force_on_bodies, integ);
+	solid_dynamics::ConstrainSolidBodyPartBySimBody
+		constraint_boulder(boulder, boulder_multibody, MBsystem, boulder_body, force_on_bodies, integ);
+
+	//-----------------------------------------------------------------------------
+	//outputs
+	//-----------------------------------------------------------------------------
+	In_Output in_output(system);
+	BodyStatesRecordingToLegacyVtk write_water_block_states(in_output, system.real_bodies_);
+	/** Output the body states for restart simulation. */
+	RestartIO restart_io(in_output, system.real_bodies_);
+	//-------------------------------------------------------------------
+	//from here the time stepping begins
+	//-------------------------------------------------------------------
+
+	/**
+	 * @brief Setup geometrics and initial conditions
+	 */
+	system.initializeSystemCellLinkedLists();
+	system.initializeSystemConfigurations();
+	wall_particles.initializeNormalDirectionFromBodyShape();
+	boulder_particles.initializeNormalDirectionFromBodyShape();
+	boulder_corrected_configuration.parallel_exec();
+	/**
+	* @brief The time stepping starts here.
+	*/
+	/** If the starting time is not zero, please setup the restart time step or read in restart states. */
+	// if (system.restart_step_ != 0)
+	// {
+	// 	GlobalStaticVariables::physical_time_ = restart_io.readRestartFiles(system.restart_step_);
+	// 	water_block.updateCellLinkedList();
+	// 	water_block_complex.updateConfiguration();
+	// }
+
+	/** Output the start states of bodies. */
+	write_water_block_states.writeToFile(0);
+
+	size_t number_of_iterations = system.restart_step_;
+	int screen_output_interval = 100;
+	int restart_output_interval = screen_output_interval * 10;
+	Real End_Time = 1.6;
+	//time step size for output file
+	Real D_Time = 0.01;
+	Real dt = 0.001; //default acoustic time step sizes
+
+	//statistics for computing time
+	tick_count t1 = tick_count::now();
+	tick_count::interval_t interval;
+
+	cout << "Start time stepping..." << endl;
+	//computation loop starts
+	while (GlobalStaticVariables::physical_time_ < End_Time)
+	{
+		Real integration_time = 0.0;
+		//integrate time (loop) until the next output time
+		while (integration_time < D_Time)
+		{
+
+			// //acceleration due to viscous force and gravity
+			// initialize_a_fluid_step.parallel_exec();
+			// Real Dt = get_fluid_advection_time_step_size.parallel_exec();
+			// Dt = SMIN(Dt, D_Time - integration_time);
+			// update_density_by_summation.parallel_exec();
+			// // viscous_acceleration.parallel_exec();
+			// // viscous_acceleration_and_transport_correction.parallel_exec();
+			// transport_velocity_correction.parallel_exec();
+
+			// Real relaxation_time = 0.0;
+			// while (relaxation_time < Dt)
+			// {
+			// 	pressure_relaxation.parallel_exec(dt);
+			// 	density_relaxation.parallel_exec(dt);
+			// 	dt = get_fluid_time_step_size.parallel_exec();
+			// 	dt = SMIN(dt, Dt - relaxation_time);
+			// 	relaxation_time += dt;
+			// 	integration_time += dt;
+			// 	GlobalStaticVariables::physical_time_ += dt;
+			// }
+
+			if (number_of_iterations % screen_output_interval == 0)
+			{
+				std::cout << std::fixed << std::setprecision(9) << "N=" << number_of_iterations << "	Time = "
+						  << GlobalStaticVariables::physical_time_
+						  << "	dt = " << dt << "\n";
+			}
+
+			
+			SimTK::State& state_for_update = integ.updAdvancedState();
+			force_on_bodies.clearAllBodyForces(state_for_update);
+			force_on_bodies.setOneBodyForce(state_for_update, boulder_body, force_on_boulder.parallel_exec());
+			integ.stepBy(dt);
+			constraint_boulder.parallel_exec();
+			integration_time += dt;
+			GlobalStaticVariables::physical_time_ += dt;
+			number_of_iterations++;
+
+			// water_block.updateCellLinkedList();
+			// water_block_complex.updateConfiguration();
+			// fluid_observer_contact.updateConfiguration();
+			// write_recorded_water_pressure.writeToFile(number_of_iterations);
+		}
+
+
+		tick_count t2 = tick_count::now();
+		write_water_block_states.writeToFile();
+		tick_count t3 = tick_count::now();
+		interval += t3 - t2;
+	}
+	tick_count t4 = tick_count::now();
+
+	tick_count::interval_t tt;
+	tt = t4 - t1 - interval;
+	std::cout << "Total wall time for computation: " << tt.seconds() << " seconds." << std::endl;
+
+	return 0;
+}
---
+++ tests/user_examples/3d_dambreak_simbody/src/Dambreak_sim.h
@@ -0,0 +1,205 @@
+/**
+ * @brief 	SPHinXsys Library.
+ */
+#include "sphinxsys.h"
+
+using namespace SPH;
+
+//for geometry
+const Real resolution_ref = 5.0e-3;	  	//particle spacing
+const Real BW = resolution_ref * 4; 	//boundary width
+const Real DL = 6.0;			  		//tank length
+const Real DH = 0.5;				  	//tank height
+const Real DW = 0.3;				  	//tank width
+const Real LL = 3.0;				  	//liquid length
+const Real LH = 0.4;				  	//liquid height
+const Real LW = 0.3;				  	//liquid width
+const Real BDL = 1.5e-2;				//boulder length
+const Real BDH = 3.0e-2;				//boulder height
+const Real BDW = 2.0e-2;				//boulder width
+const Real VWx = 4.0;					//verical wall x position
+const Real VWH = 0.2;					//verical wall height
+const Real BDx = 0.8;				  	//boulder x position
+const Real BDy = DW/2.0;				//boulder y position
+const Real BDz = 0.1;				  	//boulder z position
+
+// x="0.9" y="0.24" z="0"
+// x="0.12" y="0.12" z="0.45" 
+
+
+/** Domain bounds of the system. */
+BoundingBox system_domain_bounds(Vecd(-BW, -BW, -BW), Vecd(DL + BW, DW + BW, DH + BW));
+
+// Material properties of the fluid
+const Real rho0_f = 1000.0;
+const Real gravity_g = 9.81;
+const Real U_f = 2.0 * sqrt(gravity_g * LH);
+const Real c_f = 10.0 * U_f;
+const Real mu_f = 0.001;
+
+// Material properties of the solid.
+const Real rho0_s = 2.8e3;								/**< Boulder Density [kg/m^3] from paper. */
+const Real boulder_vol = BDL * BDH * BDW;				/**< Boulder Volume [m^3]. (1.5 x 2.0 x 3.0 cm) */
+const Real boulder_mass = rho0_s * boulder_vol;			/**< Boulder Mass [kg]. */
+const Real poisson = 0.3;								/**< Poisson's ratio. */
+const Real Youngs_modulus = 73e9;						/**< Young's modulus [Pa]. */
+
+/**
+ * @brief Contact properties of the simbody.
+ */
+const Real fK = SimTK::ContactMaterial 							/**< Stiffness Coefficient [Pa] */
+		::calcPlaneStrainStiffness(Youngs_modulus, poisson);
+const Real fDis = 10.0; // to turn off dissipation
+const Real fFac = 0.2; // to turn off friction
+const Real fVis = 0.0; //0.02; // to turn off viscous friction
+const SimTK::ContactMaterial contact_material(fK, fDis, fFac, fFac, fVis);
+const Real surface_thickness = 0.1;
+
+//	resolution which controls the quality of created polygonalmesh
+int resolution(30);
+
+void addSimbodyWallContacts(SimTK::SimbodyMatterSubsystem& matter, 
+		const SimTK::ContactCliqueId clique)
+{
+	using SimTK::YAxis;
+	using SimTK::ZAxis;
+
+	const SimTK::ContactGeometry::HalfSpace half_space;
+	// Left wall
+	const SimTK::Rotation R_left(Pi, ZAxis);
+	matter.Ground().updBody().addContactSurface(
+		SimTK::Transform(R_left),
+        SimTK::ContactSurface(half_space, contact_material)
+                       .joinClique(clique));
+	// Floor
+	const SimTK::Rotation R_floor(-Pi/2.0, YAxis);
+	matter.Ground().updBody().addContactSurface(
+		SimTK::Transform(R_floor),
+        SimTK::ContactSurface(half_space, contact_material)
+                       .joinClique(clique));
+	// Ceiling
+	const SimTK::Rotation R_ceiling(Pi/2.0, YAxis);
+	matter.Ground().updBody().addContactSurface(
+		SimTK::Transform(R_ceiling, Vec3d(0.0, 0.0, DH)),
+        SimTK::ContactSurface(half_space, contact_material)
+                       .joinClique(clique));
+	// Right wall
+	matter.Ground().updBody().addContactSurface(Vec3d(DL, 0.0, 0.0),
+        SimTK::ContactSurface(half_space, contact_material)
+                       .joinClique(clique));
+	
+	// Front wall
+	const SimTK::Rotation R_front(-Pi/2.0, ZAxis);
+	matter.Ground().updBody().addContactSurface(
+		SimTK::Transform(R_front),
+        SimTK::ContactSurface(half_space, contact_material)
+                       .joinClique(clique));
+	
+	// Back wall
+	const SimTK::Rotation R_back(Pi/2.0, ZAxis);
+	matter.Ground().updBody().addContactSurface(
+		SimTK::Transform(R_back, Vec3d(0.0, DW, 0.0)),
+		SimTK::ContactSurface(half_space, contact_material)
+					   .joinClique(clique));
+}
+
+void addCliffContactForSimbody(SimTK::SimbodyMatterSubsystem& matter,
+		const SimTK::ContactCliqueId clique) 
+{
+	Vec3d half_lengths(0.5*(DL - VWx), 0.5 * DW, 0.5 * VWH);
+	int resoluton = 2;
+
+	// Create mesh
+	SimTK::PolygonalMesh brick_mesh;
+	brick_mesh = SimTK::PolygonalMesh::createBrickMesh(half_lengths, resoluton);
+	SimTK::ContactGeometry::TriangleMesh cliff_geometry(brick_mesh);
+
+	// Add Contact surface to body
+	matter.Ground().updBody().addContactSurface(SimTK::Transform(Vec3d(VWx + 0.5*(DL - VWx), 0.5 * DW, 0.5 * VWH)),
+        SimTK::ContactSurface(cliff_geometry, contact_material, 1)
+				.joinClique(clique));
+}
+
+void addBoulderContactForSimbody(SimTK::Body::Rigid& boulder_body)
+{	
+	Vec3d half_lengths(0.5 * BDL, 0.5 * BDW, 0.5 * BDH);
+	int resolution = 2;
+
+	// Create mesh
+	SimTK::PolygonalMesh brick_mesh;
+	brick_mesh = SimTK::PolygonalMesh::createBrickMesh(half_lengths, resolution);
+	SimTK::ContactGeometry::TriangleMesh boulder_geo(brick_mesh);
+
+	boulder_body.addContactSurface(SimTK::Transform(),
+        SimTK::ContactSurface(boulder_geo, contact_material, 1));
+}
+
+//	define the fluid body
+class WaterBlock : public FluidBody
+{
+public:
+	WaterBlock(SPHSystem &system, const std::string &body_name)
+		: FluidBody(system, body_name)
+	{
+		Vecd halfsize_water(0.5 * LL, 0.5 * LW, 0.5 * LH);
+		Vecd translation_water = halfsize_water;
+
+		body_shape_.add<TriangleMeshShapeBrick>(halfsize_water, resolution, translation_water);
+	}
+};
+//	define the static solid wall boundary
+class WallBoundary : public SolidBody
+{
+public:
+	WallBoundary(SPHSystem &system, const std::string &body_name)
+		: SolidBody(system, body_name)
+	{
+		// tank shape
+		Vecd halfsize_outer(0.5 * DL + BW, 0.5 * DW + BW, 0.5 * DH + BW);
+		Vecd translation_wall(0.5 * DL, 0.5 * DW, 0.5 * DH);
+		Vecd halfsize_inner(0.5 * DL, 0.5 * DW, 0.5 * DH);
+		body_shape_.add<TriangleMeshShapeBrick>(halfsize_outer, resolution, translation_wall);
+		body_shape_.substract<TriangleMeshShapeBrick>(halfsize_inner, resolution, translation_wall);
+
+		// vertical wall
+		Vecd halfsize_vwall_outer(0.5*(DL - VWx), 0.5 * DW, 0.5 * VWH);
+		Vecd translation_vwall(VWx + 0.5*(DL - VWx), 0.5 * DW, 0.5 * VWH);
+		body_shape_.add<TriangleMeshShapeBrick>(halfsize_vwall_outer, resolution, translation_vwall);
+
+		Vecd halfsize_vwall_inner(0.5*(DL - VWx), 0.5*DW + 1.5*BW, 0.5 * VWH);
+		translation_vwall = Vecd(VWx + BW + 0.5*(DL - VWx), 0.5 * DW, -BW + 0.5*VWH);
+		body_shape_.substract<TriangleMeshShapeBrick>(halfsize_vwall_outer, resolution, translation_vwall);
+	}
+};
+
+class Boulder : public SolidBody
+{
+public:
+	Boulder(SPHSystem &system, const std::string &body_name)
+		: SolidBody(system, body_name)
+	{
+		Vecd halfsize(0.5 * BDL, 0.5 * BDW, 0.5 * BDH);
+		Vecd translation_wall(VWx - BDx + 0.5*BDL, BDy, BDz + 0.5*BDH);
+		body_shape_.add<TriangleMeshShapeBrick>(halfsize, resolution, translation_wall);
+	}
+};
+
+/**
+* @brief 	Create boulder body for simbody
+*/
+class BoulderSystemForSimbody : public SolidBodyPartForSimbody
+{
+public:
+	BoulderSystemForSimbody(SolidBody &solid_body,
+						 	const std::string &constrained_region_name,
+							Shape& shape)
+		: SolidBodyPartForSimbody(solid_body, constrained_region_name, shape)
+	{
+		body_part_mass_properties_ = mass_properties_ptr_keeper_
+			.createPtr<SimTK::MassProperties>(
+				boulder_mass, 
+				SimTK::Vec3(0.0), 
+				SimTK::UnitInertia::brick(0.5 * BDL, 0.5 * BDW, 0.5 * BDH)
+			);
+	}
+};
