c5ee879ffa93fb90393d0c92d9070f1cf4acc847
konmenel <konmenel@gmail.com>
2022-06-07 15:21:30 +0100

    Chrono ready for test with water

--- tests/user_examples/3d_dambreak_chrono/src/Chrono.h
+++ tests/user_examples/3d_dambreak_chrono/src/Chrono.h
@@ -25,39 +25,57 @@ using chrono::ChBodyEasyBox;
 using chrono::ChTimestepper;
 using chrono::ChSolver;
 
-const Real resolution_ref = 0.01;	  	//particle spacing
-const Real DL = 1.0;
-const Real DW = 1.0;
-const Real DH = 1.0;
-const Real BDL = 0.1;
-const Real BDW = 0.1;
-const Real BDH = 0.1;
-const Real Bx = DL * 0.5;
-const Real By = DW * 0.5;
-const Real Bz = DH * 0.5;
-const Real BW = 4 * resolution_ref;
+// TODO: Add the rest of the walls to chronos
+// TODO: True to create each wall of the tank seperately
+// TODO: Change tank materials
+// TODO: Tune contact coefficients
+
+//for geometry
+const Real resolution_ref = 3.75e-3;	  	//particle spacing
+const Real BW = resolution_ref * 4; 	//boundary width
+const Real DL = 4.0;			  		//tank length
+const Real DH = 0.5;				  	//tank height
+const Real DW = 0.3;				  	//tank width
+const Real LL = 1.5;				  	//liquid length
+const Real LH = 0.2;				  	//liquid height
+const Real LW = 0.3;				  	//liquid width
+const Real BDL = 3.0e-2;				//boulder length
+const Real BDH = 1.5e-2;				//boulder height
+const Real BDW = 2.0e-2;				//boulder width
+const Real VWx = 2.0;					//verical wall x position
+const Real VWH = 0.2;					//verical wall height
+const Real BDx = 0.08;				  	//boulder x position
+const Real BDy = DW * 0.5;				//boulder y position
+const Real BDz = 0.05;				  	//boulder z position
 
 const BoundingBox system_domain_bounds(Vecd(-BW, -BW, -BW), Vecd(DL + BW, DW + BW, DH + BW));
 
+// Material properties of the fluid
+const Real rho0_f = 1000.0;
+const Real gravity_g = 9.81;
+const Real U_f = 2.0 * sqrt(gravity_g * LH);
+const Real c_f = 10.0 * U_f;
+const Real mu_f = 0.001;
 
+// Material properties of the solid.
 const Real rho0_s = 2.8e3;								/**< Boulder Density [kg/m^3] from paper. */
 const Real boulder_vol = BDL * BDH * BDW;				/**< Boulder Volume [m^3]. (1.5 x 2.0 x 3.0 cm) */
 const Real boulder_mass = rho0_s * boulder_vol;			/**< Boulder Mass [kg]. */
 const Real poisson = 0.3;								/**< Poisson's ratio. */
 const Real Youngs_modulus = 73e9;						/**< Young's modulus [Pa]. */
-const Real surface_thickness = 1.0;
 
 const int resolution = 20;
 
 auto collition_model = chrono_types::make_shared<chrono::collision::ChCollisionModelBullet>();
+const float friction_coef = 0.2f;
 
 inline ChVector<> vecToCh(const Vec3d &vec) { return ChVector<>(vec[0], vec[1], vec[2]); }
 inline Vec3d vecToSim(const ChVector<> &vec) { return Vec3d(vec[0], vec[1], vec[2]); }
 
-std::shared_ptr<ChBody> addBoxCh(ChSystem &ch_system)
+std::shared_ptr<ChBody> addBoulderCh(ChSystem &ch_system)
 {
 	auto box_mat = chrono_types::make_shared<SurfMaterialCh>();
-	box_mat->SetFriction(0.4f);
+	box_mat->SetFriction(friction_coef);
 #if SMOOTH_CONTACT
 	box_mat->SetYoungModulus(Youngs_modulus);
 	box_mat->SetPoissonRatio(poisson);
@@ -72,7 +90,7 @@ std::shared_ptr<ChBody> addBoxCh(ChSystem &ch_system)
 															box_mat,
 															collition_model);
 	box_ch->SetName("Box");
-	box_ch->SetPos(ChVector<>(Bx, By, Bz));
+	box_ch->SetPos(ChVector<>(BDx, BDy, BDz));
 
 	ch_system.AddBody(box_ch);
 	return box_ch;
@@ -80,25 +98,53 @@ std::shared_ptr<ChBody> addBoxCh(ChSystem &ch_system)
 
 void addWallsCh(ChSystem &ch_system)
 {	
-	auto box_mat = chrono_types::make_shared<SurfMaterialCh>();
-	box_mat->SetFriction(0.2f);
+	auto tank_mat = chrono_types::make_shared<SurfMaterialCh>();
+	tank_mat->SetFriction(friction_coef);
 #if SMOOTH_CONTACT
-	box_mat->SetYoungModulus(Youngs_modulus);
-	box_mat->SetPoissonRatio(poisson);
+	tank_mat->SetYoungModulus(Youngs_modulus);
+	tank_mat->SetPoissonRatio(poisson);
 #endif // end if SMOOTH_CONTACT
 
-	auto floor_ch = chrono_types::make_shared<ChBodyEasyBox>(	DL,
+	auto floor1_ch = chrono_types::make_shared<ChBodyEasyBox>(	VWx,
 																DW,
 																BW,
 																rho0_s,
 																false,
 																true,
-																box_mat);
-	floor_ch->SetName("Floor");
-	floor_ch->SetPos(ChVector<>(DL * 0.5, DW * 0.5, -BW * 0.5));
-	floor_ch->SetBodyFixed(true);
+																tank_mat,
+																collition_model);
+	floor1_ch->SetName("Floor1");
+	floor1_ch->SetPos(ChVector<>(VWx * 0.5, DW * 0.5, -BW * 0.5));
+	floor1_ch->SetBodyFixed(true);
+	ch_system.Add(floor1_ch);
 
-	ch_system.AddBody(floor_ch);
+
+	auto floor2_ch = chrono_types::make_shared<ChBodyEasyBox>(	DL - VWx,
+																DW,
+																BW,
+																rho0_s,
+																false,
+																true,
+																tank_mat,
+																collition_model);
+	floor2_ch->SetName("Floor2");
+	floor2_ch->SetPos(ChVector<>((DL - VWx)*0.5, DW * 0.5, VWH - BW*0.5));
+	floor2_ch->SetBodyFixed(true);
+	ch_system.Add(floor2_ch);
+
+	auto verical_wall_ch = chrono_types
+		::make_shared<ChBodyEasyBox>(	BW,
+										DW,
+										VWH,
+										rho0_s,
+										false,
+										true,
+										tank_mat,
+										collition_model);
+	verical_wall_ch->SetName("Cliff");
+	verical_wall_ch->SetPos(ChVector<>(VWx + BW*0.5, DW * 0.5, VWH*0.5 - BW));
+	verical_wall_ch->SetBodyFixed(true);
+	ch_system.Add(verical_wall_ch);
 	return;
 }
 
@@ -108,26 +154,49 @@ public:
 	WallBoundary(SPHSystem &system, const std::string &body_name)
 		: SolidBody(system, body_name)
 	{
+		// tank shape
 		Vecd halfsize_outer(0.5*DL + BW, 0.5*DW + BW, 0.5*DH + BW);
 		Vecd translation_outer(0.5*DL, 0.5*DW, 0.5*DH);
-		Vecd halfsize_inner(0.5*DL + BW, 0.5*DW, 0.5*DH);
-		Vecd translation_inner(0.5*DL, 0.5*DW, 0.5*DH);
+		Vecd halfsize_linner(0.5*VWx, 0.5*DW, 0.5*DH);
+		Vecd translation_linner(0.5*VWx, 0.5*DW, 0.5*DH);
+		Vecd halfsize_rinner(0.5*(DL - VWx), 0.5*DW, 0.5*(DH - VWH));
+		Vecd translation_rinner(VWx + 0.5*(DL - VWx), 0.5*DW, VWH + 0.5*(DH - VWH));
 
 		body_shape_.add<TriangleMeshShapeBrick>(halfsize_outer, resolution, translation_outer);
-		body_shape_.substract<TriangleMeshShapeBrick>(halfsize_inner, resolution, translation_inner);
+		body_shape_.substract<TriangleMeshShapeBrick>(halfsize_linner, resolution, translation_linner);
+		body_shape_.substract<TriangleMeshShapeBrick>(translation_rinner, resolution, translation_rinner);
+
+		// vertical wall remove inside
+		Vecd halfsize_wall_inner(0.5*(DL - VWx + BW), 0.5*DW + BW, 0.5*(VWH + BW));
+		Vecd translation_wall_inner(VWx + 0.5*(DL - VWx) + BW, 0.5*DW, 0.5*VWH - BW);
+		body_shape_.substract<TriangleMeshShapeBrick>(halfsize_wall_inner, resolution, translation_wall_inner);
 	}
 };
 
-class Box : public SolidBody
+//	define the fluid body
+class WaterBlock : public FluidBody
 {
 public:
-	Box(SPHSystem &system, const std::string &body_name)
+	WaterBlock(SPHSystem &system, const std::string &body_name)
+		: FluidBody(system, body_name)
+	{
+		Vecd halfsize_water(0.5 * LL, 0.5 * LW, 0.5 * LH);
+		Vecd translation_water(0.5 * LL, 0.5 * LW, 0.5 * LH);
+
+		body_shape_.add<TriangleMeshShapeBrick>(halfsize_water, resolution, translation_water);
+	}
+};
+
+class Boulder : public SolidBody
+{
+public:
+	Boulder(SPHSystem &system, const std::string &body_name)
 		: SolidBody(system, body_name)
 	{
-		Vecd halfsize(0.5*BDL, 0.5*BDW, 0.5*BDH);
-		Vecd translation(Bx, By, Bz);
+		Vecd halfsize(0.5 * BDL, 0.5 * BDW, 0.5 * BDH);
+		Vecd translation_wall(VWx - BDx - 0.5*BDL, BDy, BDz + 0.5*BDH);
 
-		body_shape_.add<TriangleMeshShapeBrick>(halfsize, resolution, translation);
+		body_shape_.add<TriangleMeshShapeBrick>(halfsize, 0, translation_wall);
 	}
 };
 
--- tests/user_examples/3d_dambreak_chrono/src/Dambreak_ch.cpp
+++ tests/user_examples/3d_dambreak_chrono/src/Dambreak_ch.cpp
@@ -6,6 +6,8 @@
 * ---------------------------------------------------------------------------*/
 #include "Chrono.h"
 
+#define ENABLE_WATER 1
+
 
 int main()
 {
@@ -13,25 +15,67 @@ int main()
 	LogOutput fcout("Run.out");
 	GlobalStaticVariables::physical_time_ = 0.0;
 
+#if ENABLE_WATER
+	//the water block
+	WaterBlock water_block(system, "WaterBody");
+	// create fluid particles
+	FluidParticles fluid_particles(water_block, makeShared<WeaklyCompressibleFluid>(rho0_f, c_f, mu_f));
+#endif //ENABLE_WATER
+
 	WallBoundary wall_boundary(system, "Wall");
 	SolidParticles wall_particles(wall_boundary);
 
-	Box box(system, "Box");
-	ElasticSolidParticles boulder_particles(box, makeShared<LinearElasticSolid>(rho0_s, poisson, Youngs_modulus));
+	Boulder boulder(system, "Boulder");
+	ElasticSolidParticles boulder_particles(boulder, makeShared<LinearElasticSolid>(rho0_s, poisson, Youngs_modulus));
+
+	/** topology */
+	BodyRelationInner boulder_inner(boulder);
+#if ENABLE_WATER
+	ComplexBodyRelation water_block_complex(water_block, {&wall_boundary, &boulder});
+#endif //ENABLE_WATER
+
+	solid_dynamics::CorrectConfiguration boulder_corrected_configuration(boulder_inner);
+
+	//-------------------------------------------------------------------
+	//this section define all numerical methods will be used in this case
+	//-------------------------------------------------------------------
+	//-------- common particle dynamics ----------------------------------------
+	Gravity gravity(Vec3d(0.0, 0.0, -gravity_g));
+#if ENABLE_WATER
+	TimeStepInitialization initialize_a_fluid_step(water_block, gravity);
+	//-------- fluid dynamics --------------------------------------------------
+	//evaluation of density by summation approach
+	fluid_dynamics::DensitySummationFreeSurfaceComplex update_density_by_summation(water_block_complex);
+	//time step size without considering sound wave speed
+	fluid_dynamics::AdvectionTimeStepSize get_fluid_advection_time_step_size(water_block, U_f);
+	//time step size with considering sound wave speed
+	fluid_dynamics::AcousticTimeStepSize get_fluid_time_step_size(water_block);
+
+	//pressure relaxation using verlet time stepping
+	fluid_dynamics::PressureRelaxationWithWall pressure_relaxation(water_block_complex);
+	fluid_dynamics::DensityRelaxationWithWall density_relaxation(water_block_complex);
+	/** Computing viscous acceleration. */
+	fluid_dynamics::ViscousAccelerationWithWall viscous_acceleration(water_block_complex);
+	/** Impose transport velocity. */
+	// fluid_dynamics::TransportVelocityCorrectionComplex transport_velocity_correction(water_block_complex);
+	/** viscous acceleration and transport velocity correction can be combined because they are independent dynamics. */
+	// CombinedInteractionDynamics viscous_acceleration_and_transport_correction(viscous_acceleration, transport_velocity_correction);
+#endif //ENABLE_WATER
 
 	fcout << "Setting up Chrono..." << std::flush;
 	SystemCh ch_system;
+	GlobalStaticVariables::physical_time_ = 0.0;
 	
-	std::shared_ptr<ChBody> box_ch = addBoxCh(ch_system);
+	auto boulder_ch = addBoulderCh(ch_system);
 	addWallsCh(ch_system);
 
 	ch_system.SetTimestepperType(ChTimestepper::Type::EULER_IMPLICIT_LINEARIZED);
 	ch_system.SetSolverType(ChSolver::Type::PSOR);
 	ch_system.SetSolverMaxIterations(50);
-	ch_system.Set_G_acc(ChVector<>(0.0, 0.0, -9.81));
+	ch_system.Set_G_acc(ChVector<>(0.0, 0.0, -gravity_g));
 
-	BoulderSystemForChrono box_for_chrono(box, "Box_chrono", box.body_shape_);
-	ConstrainSolidBodyPartByChrono box_constain(box, box_for_chrono, box_ch);
+	BoulderSystemForChrono boulder_for_chrono(boulder, "Boulder_chrono", boulder.body_shape_);
+	ConstrainSolidBodyPartByChrono box_constain(boulder, boulder_for_chrono, boulder_ch);
 
 	fcout << "OK!" << endl;
 
@@ -39,32 +83,85 @@ int main()
 	In_Output in_output(system);
 	BodyStatesRecordingToLegacyVtk write_body_states(in_output, system.real_bodies_);
 
-	size_t step = 0;
+	/**
+	 * @brief Setup geometrics and initial conditions
+	 */
+	system.initializeSystemCellLinkedLists();
+	system.initializeSystemConfigurations();
+	wall_particles.initializeNormalDirectionFromBodyShape();
+	boulder_particles.initializeNormalDirectionFromBodyShape();
+	boulder_corrected_configuration.parallel_exec();
+
+	size_t number_of_iterations = 0;
 	Real dt = 0.001;
-	Real end_time = 10.0;
-	Real real_time = 0.0;
-	Real out_time = 0.02;
+	Real end_time = 2.0;
+	Real out_time = 0.01;
 	size_t report_steps = 500;
 
 	write_body_states.writeToFile(0);
 
-	while (real_time < end_time) {
+	//statistics for computing time
+	tick_count t1 = tick_count::now();
+	tick_count::interval_t interval;
+
+	fcout << "Main loop started..." << endl;
+	while (GlobalStaticVariables::physical_time_ < end_time) {
 		Real integration_time = 0.0;
 		while (integration_time < out_time) {
-			if (step % report_steps == 0) {
-				fcout << "Step = " << step << "\treal time = " << real_time << endl;
-			}
+#if ENABLE_WATER
+			// acceleration due to viscous force and gravity
+			initialize_a_fluid_step.parallel_exec();
+			Real Dt = get_fluid_advection_time_step_size.parallel_exec();
+			Dt = SMIN(Dt, out_time - integration_time);
+			update_density_by_summation.parallel_exec();
+			viscous_acceleration.parallel_exec();
+			// transport_velocity_correction.parallel_exec();
+			// viscous_acceleration_and_transport_correction.parallel_exec();
+#else
+			Real Dt = 0.005;
+#endif // ENABLE_WATER
+
+			Real relaxation_time = 0.0;
+			while (relaxation_time < Dt) {
+#if ENABLE_WATER
+				pressure_relaxation.parallel_exec(dt);
+				density_relaxation.parallel_exec(dt);
+				dt = get_fluid_time_step_size.parallel_exec();
+				dt = SMIN(dt, Dt - relaxation_time);
+#endif // ENABLE_WATER
 
 			ch_system.DoStepDynamics(dt);
 			box_constain.parallel_exec();
-
+			
 			integration_time += dt;
-			real_time += dt;
-			step++;
-		}
-		write_body_states.writeToFile(step);
-	}
+			relaxation_time += dt;
+			GlobalStaticVariables::physical_time_ += dt;
+			}
 
+			if (number_of_iterations % report_steps == 0) {
+				fcout << "Step = " << number_of_iterations << "\treal time = " << 
+				GlobalStaticVariables::physical_time_ << endl;
+			}
+
+			number_of_iterations++;
+
+#if ENABLE_WATER
+			water_block.updateCellLinkedList();
+			water_block_complex.updateConfiguration();
+#endif // ENABLE_WATER
+		}
+
+		tick_count t2 = tick_count::now();
+		write_body_states.writeToFile(number_of_iterations);
+		tick_count t3 = tick_count::now();
+		interval += t3 - t2;
+	}
+	tick_count t4 = tick_count::now();
+
+	tick_count::interval_t tt;
+	tt = t4 - t1 - interval;
+	fcout << "Total wall time for computation: " << tt.seconds() << " seconds." << std::endl;
+	fcout.close();
 
 	return 0;
 }
